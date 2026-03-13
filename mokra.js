af2d8146a3ed31c33f4f166ab56bfd24090877f0
Peter Nsaka <nsakapeter@gmail.com>
2026-03-13 13:11:09 -0400

    Add script

---
+++ mokra.js
@@ -0,0 +1,10 @@
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://app.mokra.ai/embed.js';
+  script.setAttribute('data-service', 'loop-returns');
+  script.setAttribute('data-position', 'bottom-right');
+  script.setAttribute('data-button-text', 'Try the API');
+  script.setAttribute('data-button-color', '#000000');
+  script.setAttribute('data-theme', 'light');
+  document.head.appendChild(script);
+})();
