10c42a6567aeaa9a6c8360b869509cda7c4e00b2
Peter Nsaka <nsakapeter@gmail.com>
2026-03-13 13:48:53 -0400

    Add script

--- mokra.js
+++ mokra.js
@@ -4,7 +4,7 @@
   script.setAttribute('data-service', 'loop-returns');
   script.setAttribute('data-position', 'bottom-right');
   script.setAttribute('data-button-text', 'Try On Mock Server');
-  script.setAttribute('data-button-color', '#1e90ed');
+  script.setAttribute('data-button-color', '#0088ff');
   script.setAttribute('data-theme', 'light');
   document.head.appendChild(script);
 })();
