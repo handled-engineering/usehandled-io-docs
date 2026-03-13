922f7448f936bbc1a3c802229a44f030ee074db6
Peter Nsaka <nsakapeter@gmail.com>
2026-03-13 14:02:59 -0400

    Add script

--- mokra.js
+++ mokra.js
@@ -3,7 +3,7 @@
   script.src = 'https://app.mokra.ai/embed.js';
   script.setAttribute('data-service', 'loop-returns');
   script.setAttribute('data-position', 'bottom-right');
-  script.setAttribute('data-button-text', 'Try On Mock Server');
+  script.setAttribute('data-button-text', 'Create Mock Server');
   script.setAttribute('data-button-color', '#0088ff');
   script.setAttribute('data-theme', 'light');
   document.head.appendChild(script);
